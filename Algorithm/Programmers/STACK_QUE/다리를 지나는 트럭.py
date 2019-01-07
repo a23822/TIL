ab62e00cabf58bfe2fc79f531ca4c2f3ced2fda1
chanwoochae <a23842@naver.com>
2019-01-07 18:01:01 +0900

    2019-01-07, 자력으로 못 품(시간초과)

--- Algorithm/Programmers/STACK_QUE/다리를 지나는 트럭.py
+++ Algorithm/Programmers/STACK_QUE/다리를 지나는 트럭.py
@@ -39,3 +39,119 @@ def solution(bridge_length, weight, truck_weights):
         # print("현재상황은 {}".format(ing))
         # print("다리를 건너는 트럭은 {}".format(onBridge))
     return cnt+1
+
+
+"""
+다른사람풀이
+"""
+
+def solution2(bridge_length, weight, truck_weights):
+    ing = [0]*bridge_length
+    cnt = 0
+    while ing:
+        cnt += 1
+        ing.pop(0)#트럭이 지나감
+        if truck_weights:
+            if sum(ing)+truck_weights[0]<=weight:#다리에 있는 트럭의 무게와 대기 중인 트럭의 무게가 제한 무게보다 작거나 같다면
+                ing.append(truck_weights.pop(0))#다리에 트럭 올림
+            else:
+                ing.append(0)#중량 초과면 트럭 안올림
+    return cnt
+
+"""
+정통파??
+"""
+
+import collections
+
+DUMMY_TRUCK = 0
+
+
+class Bridge(object):
+
+    def __init__(self, length, weight):
+        self._max_length = length
+        self._max_weight = weight
+        self._queue = collections.deque()
+        self._current_weight = 0
+
+    def push(self, truck):
+        next_weight = self._current_weight + truck
+        if next_weight <= self._max_weight and len(self._queue) < self._max_length:
+            self._queue.append(truck)
+            self._current_weight = next_weight
+            return True
+        else:
+            return False
+
+    def pop(self):
+        item = self._queue.popleft()
+        self._current_weight -= item
+        return item
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __repr__(self):
+        return 'Bridge({}/{} : [{}])'.format(self._current_weight, self._max_weight, list(self._queue))
+
+
+def solution3(bridge_length, weight, truck_weights):
+    bridge = Bridge(bridge_length, weight)
+    trucks = collections.deque(w for w in truck_weights)
+
+    for _ in range(bridge_length):
+        bridge.push(DUMMY_TRUCK)
+
+    count = 0
+    while trucks:
+        bridge.pop()
+
+        if bridge.push(trucks[0]):
+            trucks.popleft()
+        else:
+            bridge.push(DUMMY_TRUCK)
+
+        count += 1
+
+    while bridge:
+        bridge.pop()
+        count += 1
+
+    return count
+
+
+def main():
+    print(solution(2, 10, [7, 4, 5, 6]), 8)
+    print(solution(100, 100, [10]), 101)
+    print(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]), 110)
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+FIFO
+"""
+
+def solution4(bridge_length, max_weight, truck_weights):
+    # FIFO 문제.
+    bridge = [0]*bridge_length
+    curr_weight = 0
+    ans = 0
+    while len(truck_weights) > 0:
+        ans += 1
+        ar = bridge.pop(0)
+        curr_weight -= ar
+        if curr_weight + truck_weights[0] > max_weight:
+            bridge.append(0)            
+        else:
+            truck = truck_weights.pop(0)
+            bridge.append(truck)
+            curr_weight += truck
+    while curr_weight > 0:
+        ans += 1
+        ar = bridge.pop(0)
+        curr_weight -= ar
+    return ans
