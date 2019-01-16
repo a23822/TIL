82dbbcfc57b9de4d92b561e7017ec99fcc77bd12
chanwoochae <a23842@naver.com>
2019-01-16 17:54:23 +0900

    2019-01-16

--- Algorithm/Programmers/완전탐색/모의고사.py
+++ Algorithm/Programmers/완전탐색/모의고사.py
@@ -1,6 +1,4 @@
 def solution(answers):
-    answer = []
-    print(answers)
     """
     1번 수포자 :  12345 반복
     2번 수포자 :  21 23 24 25 반복
@@ -9,5 +7,16 @@ def solution(answers):
     supo1 = [1, 2, 3, 4, 5]
     supo2 = [2, 1, 2, 3, 2, 4, 2, 5]
     supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    
-    return answer
+    info = [0, 0, 0]
+    for i in range(len(answers)):
+        if answers[i] == supo1[i]:
+            info[0] += 1
+        if answers[i] == supo2[i]:
+            info[1] += 1
+        if answers[i] == supo3[i]:
+            info[2] += 1
+    res = []
+    for i in range(len(info)):
+        if info[i] == max(info):
+            res.append(i+1)
+    return res
