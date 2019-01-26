89aec22ed97ee5838d92fc0a9a5b8c459c0d89fc
a23822 <a23842@naver.com>
2019-01-27 00:15:43 +0900

    2019-01-27

--- Algorithm/BaekJoon/9498_시험성적.py
+++ Algorithm/BaekJoon/9498_시험성적.py
@@ -18,11 +18,11 @@ score = int(input())
 
 if score >= 90 and score <= 100:
     print("A")
-elif score > 80:
+elif score >= 80:
     print("B")
-elif score > 70:
+elif score >= 70:
     print("C")
-elif score > 60:
+elif score >= 60:
     print('D')
 else:
     print("F")
