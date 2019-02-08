665265c6d5042ad373510d3e374a97f914fa3d54
채찬우 <a23842@naver.com>
2019-02-08 17:41:03 +0900

    2019-02-08

--- Algorithm/Programmers/이분탐색/예산.py
+++ Algorithm/Programmers/이분탐색/예산.py
@@ -1,13 +1,21 @@
 def solution(budgets, M):
-    answer = 0
     n_list = []
     sm = min(budgets)
     bg = max(budgets)
+    chk = 0
+    for b in budgets:
+        chk += b
+    
+    if  chk < M:
+        return int(M/len(budgets))
+    
     while True:
+        temp1 = budgets
         md = int((sm+bg)/2)
+        n_list.append(md)
         #print(f'작은건 {sm} 큰건 {bg}')
         n_sum = 0
-        for b in budgets:
+        for b in temp1:
             if b > md:
                 b = md
             n_sum += b
@@ -15,10 +23,17 @@ def solution(budgets, M):
             bg = md
         elif n_sum < M:
             sm = md
-        if sm+1 == bg:
+        if sm+1 == bg or sm == bg:
             break
-        elif sm == bg:
-            break
-        n_list.append(md)
-    #print(n_list)
-    return n_list[-1]
+        
+    temp2 = budgets
+    n_list2 = []
+    for n in n_list:
+        sum = 0
+        for b in temp2:
+            if b > n:
+                b = n
+            sum += b
+        n_list2.append(abs(sum-M))
+    answer = n_list[n_list2.index(min(n_list2))]
+    return answer
